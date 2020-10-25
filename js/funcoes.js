6ec036f5e5566e6592f9f08186f45216d538bf61
Arigariso <arigariso@gmail.com>
2020-10-25 11:13:17 -0300

    updade full stack eletro

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,14 +1,14 @@
 function exibir_categoria(categoria) {
-    let elementos = document.getElementsByClassName('box_produto');
+    let elementos = document.getElementsByClassName('box_produto')
 
     for (var i = 0; i < elementos.length; i++) {
-        /* console.log(elementos[i].id); */
-        if (categoria == elementos[i].id)
-            elementos[i].style = "display: inline-block";
+        if (categoria == elementos[i].id) 
+            elementos[i].style = "display: inline-block;"
         else
             elementos[i].style = "display:none";
     }
 }
+
 /* arrow function */
 
 let exibir_todos = () => {
@@ -19,15 +19,37 @@ let exibir_todos = () => {
     }
 }
 
-let destaque = (imagem) => {
-    console.log(imagem);
-   if (imagem.width == 120) {
-       imagem.width = 240;
-   }
-   else if (imagem.width == 240) {
-       imagem.width = 120;
-   }
-   else {
-       imagem.width = 120;
-   }
+let mouseOver = () => {
+  
+    let elementos = document.getElementsByTagName('img');
+
+    for (var i = 0; i < elementos.length; i++) {      
+        elementos[i].style = "cursor:pointer";
+    }  
 }
+
+let mouseLista = () => {
+  
+    let elementos = document.getElementsByTagName('li');
+
+    for (var i = 0; i < elementos.length; i++) {      
+        elementos[i].style = "cursor: pointer; ";
+    } 
+}
+
+function destaque(imagem) {
+    /* console.log(imagem); */
+    if (imagem.width == 120) {
+        imagem.width = 240;
+    }
+    else if (imagem.width == 240) {
+        imagem.width = 120;
+    }
+    else {
+        imagem.width = 120;
+    }
+}
+
+
+
+
